ea96de80884dd78c5b21cb3201b23940bdc9d30e
iamkroot <kroot.patel@gmail.com>
2019-10-25 13:05:44 +0530

    users: Add base forms and admin control for CustomUser

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ("name", "email", "is_staff", "is_active")
+    list_filter = ("email", "is_staff", "is_active")
+    fieldsets = (
+        (None, {"fields": ("name", "email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "name",
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
+    )
+    search_fields = ("name", "email")
+    ordering = ("name", "email")
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
