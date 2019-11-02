b010ad742ab81dd531fa645cc9c5a9b713128cf2
Ujjwal Raizada <ujjwalraizada@gmail.com>
2019-11-02 10:09:54 +0530

    DRF: Add initial configurations

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -34,7 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-    'research'
+    'research',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -98,6 +99,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
