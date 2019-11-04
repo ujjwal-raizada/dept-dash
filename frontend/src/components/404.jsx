50316a70deb2f47195d53f7a2437885f6686b98f
iamkroot <kroot.patel@gmail.com>
2019-11-04 15:30:21 +0530

    frontend: Add Router, LazyLoader

---
+++ frontend/src/components/404.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class ErrorPage404 extends Component {
+  render() {
+    return (
+      <div className="home">
+        <h1>Uh oh, that's a 404</h1>
+        <h1>We weren't able to find that page for you</h1>
+      </div>
+    );
+  }
+}
