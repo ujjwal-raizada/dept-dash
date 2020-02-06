6e0372e972333f72ca3cb6ad0679a14dcf218866
iamkroot <kroot.patel@gmail.com>
2020-02-06 16:10:47 +0530

    backend: Use PKRelatedFields instead of nested serializers
    The earlier approach in serializing the Project and Publication objects
    was to have the related author fields as nested serializations. This
    approach was wrong, as it created problems when trying to create/update
    the objects, since the nested serializers required the entire objects.
    Now we use the PrimaryKeyRelatedField, as recommended by the docs, and
    manually populate the nested related fields during serialization.

--- backend/research/serializers.py
+++ backend/research/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Project, Publication
+from users.models import Faculty, Student, ResearchScholar
 from users.serializers import (
     FacultySerializer,
     StudentSerializer,
@@ -7,14 +8,52 @@ from users.serializers import (
 )
 
 
+def populate_related(data, field, Serializer):
+    """Converts (list of) pk(s) to the serialized representation"""
+    Model = Serializer.Meta.model
+    if isinstance(data[field], list):
+        many = True
+        objects = Model.objects.filter(pk__in=data[field])
+    else:
+        many = False
+        objects = Model.objects.filter(pk=data[field])
+    data[field] = Serializer(objects, many=many).data
+
+
 class ResearchWorkSerializer(serializers.ModelSerializer):
-    faculty_authors = FacultySerializer(many=True)
-    student_authors = StudentSerializer(many=True)
-    scholar_authors = ResearchScholarSerializer(many=True)
+    faculty_authors = serializers.PrimaryKeyRelatedField(
+        queryset=Faculty.objects.all(), many=True, required=False
+    )
+    student_authors = serializers.PrimaryKeyRelatedField(
+        queryset=Student.objects.all(), many=True, required=False
+    )
+    scholar_authors = serializers.PrimaryKeyRelatedField(
+        queryset=ResearchScholar.objects.all(), many=True, required=False
+    )
+
+    def validate(self, data):
+        author_fields = ("faculty_authors", "student_authors", "scholar_authors")
+        if all(not data.get(field, True) for field in author_fields):
+            raise serializers.ValidationError("At least one author is required")
+        return data
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        populate_related(data, "faculty_authors", FacultySerializer)
+        populate_related(data, "student_authors", StudentSerializer)
+        populate_related(data, "scholar_authors", ResearchScholarSerializer)
+        return data
 
 
 class ProjectSerializer(ResearchWorkSerializer):
-    proposed_by = FacultySerializer(many=True, read_only=True)
+    proposed_by = serializers.PrimaryKeyRelatedField(
+        queryset=Faculty.objects.all(), many=True
+    )
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        populate_related(data, "proposed_by", FacultySerializer)
+        return data
 
     class Meta:
         model = Project
